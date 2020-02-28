82fc9de2a5a3824822bfb5e9bbfe5db0ab60781b
chatton <cianhatton@gmail.com>
2020-02-28 15:30:23 +0000

    wip

--- scripts/ci/generate_k8.sh
+++ scripts/ci/generate_k8.sh
@@ -6,4 +6,11 @@ set -xeo pipefail
 # GOROOT needs to be explicitly set otherwise we get the error
 # time="2020-02-24T17:52:08Z" level=info msg="Running deepcopy code-generation for Custom Resource group versions: [mongodb:[v1], ]\n"
 # deepcopy.go:885] Hit an unsupported type invalid type for invalid type, from ./pkg/apis/mongodb/v1.MongoDB
-GOROOT="/opt/golang/go1.13" operator-sdk generate k8s
+#GOROOT="/opt/golang/go1.13" operator-sdk generate k8s
+operator-sdk generate k8s
+#echo "using GOROOT=${GOROOT}"
+#go version
+#go env
+#
+#unset GOPATH
+#operator-sdk generate k8s
--- scripts/ci/run_e2e_tests.sh
+++ scripts/ci/run_e2e_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -o nounset
-set -xeo pipefail
+#set -o nounset
+#set -xeo pipefail
 
 operator-sdk test local ./test/e2e --kubeconfig "${KUBECONFIG}" --namespace default --up-local
