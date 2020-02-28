6ef53cab5c3e64c28423a26c44c791ae92f843a4
chatton <cianhatton@gmail.com>
2020-02-28 19:04:09 +0000

    working go modules!

--- scripts/ci/run_e2e_tests.sh
+++ scripts/ci/run_e2e_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-#set -o nounset
-#set -xeo pipefail
+set -o nounset
+set -xeo pipefail
 
+go mod vendor
 operator-sdk test local ./test/e2e --kubeconfig "${KUBECONFIG}" --namespace default --up-local
