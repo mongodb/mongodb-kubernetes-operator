5cb2d67c451ddc2c878b710ba0eb5b79464b8592
chatton <cianhatton@gmail.com>
2020-03-02 16:13:25 +0000

    removed commented code

--- scripts/ci/run_e2e_tests.sh
+++ scripts/ci/run_e2e_tests.sh
@@ -4,4 +4,5 @@ set -o nounset
 set -xeo pipefail
 
 go mod vendor
+# we run the tests in verbose mode to ensure we get all test logs displayed
 operator-sdk test local ./test/e2e --kubeconfig "${KUBECONFIG}" --namespace default --verbose
