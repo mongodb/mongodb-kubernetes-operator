8314307343e38f952c567516da0597081504e207
chatton <cianhatton@gmail.com>
2020-03-04 14:19:22 +0000

    wip

--- scripts/ci/build_and_push_operator.sh
+++ scripts/ci/build_and_push_operator.sh
@@ -2,6 +2,6 @@
 
 echo ${quay_password} | docker login -u=${quay_user_name} quay.io --password-stdin
 
-docker build . -f Dockerfile-operator -t quay.io/mongodb/community-operator-dev:${revision}
+docker build . -f docker/Dockerfile.operator -t quay.io/mongodb/community-operator-dev:${revision}
 
 docker push quay.io/mongodb/community-operator-dev:${revision}
