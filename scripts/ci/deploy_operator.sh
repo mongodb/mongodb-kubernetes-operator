820c51068ae04addbccd8da7454bc1631e3b1396
chatton <cianhatton@gmail.com>
2020-03-06 17:08:13 +0000

    reverted evergreen changes

--- scripts/ci/deploy_operator.sh
+++ scripts/ci/deploy_operator.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-go run scripts/ci/e2e/e2e.go --kubeconfig ${KUBECONFIG} --operatorImage quay.io/mongodb/community-operator-dev:${version_id}
+kubectl apply -f deploy/crds/*crd.yaml -n default
+kubectl apply -f deploy -n default
+
+# TODO: Temporary mechanism to update the image of the deployment before a templating mechanism is in place
+kubectl set image deployment/mongodb-kubernetes-operator mongodb-kubernetes-operator=quay.io/mongodb/community-operator-dev:${version_id}
