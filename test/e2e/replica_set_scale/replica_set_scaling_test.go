6dc45a13f49708f40d7314c8225a3985d52dfe08
chatton <cianhatton@gmail.com>
2020-03-12 19:54:05 +0000

    added comments, removed dead code

--- test/e2e/replica_set_scale/replica_set_scaling_test.go
+++ test/e2e/replica_set_scale/replica_set_scaling_test.go
@@ -26,7 +26,7 @@ func TestReplicaSetScale(t *testing.T) {
 	t.Run("Create MongoDB Resource", mongodbtests.CreateOrUpdateResource(&mdb, ctx))
 	t.Run("Config Map Was Correctly Created", mongodbtests.AutomationConfigConfigMapExists(&mdb))
 	t.Run("Stateful Set Reaches Ready State", mongodbtests.StatefulSetIsReady(&mdb))
-	t.Run("IsReachableDuring", mongodbtests.IsReachableDuring(&mdb,
+	t.Run("MongoDB is reachable", mongodbtests.IsReachableDuring(&mdb,
 		func() {
 			t.Run("Scale MongoDB Resource Up", mongodbtests.Scale(&mdb, 5, ctx))
 			t.Run("Stateful Set Scaled Up Correctly", mongodbtests.StatefulSetIsReady(&mdb))
