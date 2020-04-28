12d73a662a47ac4cb209fd7b44dbeba27294449c
chatton <cianhatton@gmail.com>
2020-04-28 13:08:31 +0100

    Created Jinja Templates

--- scripts/ci/build_and_push_image.sh
+++ scripts/ci/build_and_push_image.sh
@@ -2,5 +2,6 @@
 
 echo ${quay_password} | docker login -u=${quay_user_name} quay.io --password-stdin
 
+python docker/dockerfile_generator.py ${image_type} > Dockerfile
 docker build . -f ${dockerfile} -t ${image}
 docker push ${image}
