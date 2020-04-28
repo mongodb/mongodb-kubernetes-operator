4324655d4cb088a9c8275262a4cb365190e2dcd7
chatton <cianhatton@gmail.com>
2020-04-28 13:11:19 +0100

    added dockerfile_generator python script

---
+++ docker/dockerfile_generator.py
@@ -0,0 +1,67 @@
+import jinja2
+import argparse
+
+
+def operator_params():
+    return {
+        "builder": True,
+        "builder_image": "golang:alpine",
+        "base_image": "registry.access.redhat.com/ubi8/ubi-minimal:latest",
+    }
+
+
+def test_runner_params():
+    return {
+        "builder": True,
+        "builder_image": "golang:alpine",
+        "base_image": "registry.access.redhat.com/ubi8/ubi-minimal:latest",
+    }
+
+
+def e2e_params():
+    return {
+        "base_image": "golang:alpine",
+    }
+
+
+def unit_test_params():
+    return {
+        "base_image": "golang:alpine",
+    }
+
+
+def render(image_name: str):
+    param_dict = {
+        "unittest": unit_test_params(),
+        "e2e": e2e_params(),
+        "testrunner": test_runner_params(),
+        "operator": operator_params(),
+    }
+
+    if image_name not in param_dict:
+        raise ValueError(
+            "Image name: {} is invalid. Valid values are {}".format(
+                image_name, param_dict.keys()
+            )
+        )
+
+    env = jinja2.Environment()
+    env.loader = jinja2.FileSystemLoader(searchpath="docker/templates")
+    return env.get_template("Dockerfile.{}".format(image_name)).render(
+        param_dict[image_name]
+    )
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image", help="Type of image for the Dockerfile")
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    print(render(args.image))
+
+
+if __name__ == "__main__":
+    main()
