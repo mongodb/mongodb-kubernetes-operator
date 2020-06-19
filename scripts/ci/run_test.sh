bcda22c3123815bf310b3a7b52a64e9fbf9b44ac
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-06-19 15:56:19 +0100

    wip

--- scripts/ci/run_test.sh
+++ scripts/ci/run_test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+PY_VERSION=`python3 --version`
+echo $PY_VERSION
 SKIP_CLEANUP="1" python3 ./scripts/dev/e2e.py --skip-image-build 1 --test ${test:?} --tag ${version_id:?} --config_file ./scripts/ci/config.json
