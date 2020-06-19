920c6937d999b0dd403adc8f551df758dfca3c2c
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-06-19 16:33:42 +0100

    requirements

--- scripts/ci/run_test.sh
+++ scripts/ci/run_test.sh
@@ -2,4 +2,5 @@
 
 PY_VERSION=`python3 --version`
 echo $PY_VERSION
+pip install -r ./requirements.txt
 SKIP_CLEANUP="1" python3 ./scripts/dev/e2e.py --skip-image-build 1 --test ${test:?} --tag ${version_id:?} --config_file ./scripts/ci/config.json
