d19a96c747905b03969b8f940abb70e5097a1bb8
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-06-19 15:30:26 +0100

    fix

--- scripts/ci/run_test.sh
+++ scripts/ci/run_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-SKIP_CLEANUP="1" python ./scripts/dev/e2e.py --test ${test:?} --tag ${version_id:?} --config_file ./scripts/ci/config.json 
+SKIP_CLEANUP="1" python3 ./scripts/dev/e2e.py --test ${test:?} --tag ${version_id:?} --config_file ./scripts/ci/config.json 
 
