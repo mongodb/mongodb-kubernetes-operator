d182801f980a38d53b67172de90b45737c35e709
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-06-22 20:02:37 +0100

    wip

--- scripts/ci/run_test.sh
+++ scripts/ci/run_test.sh
@@ -4,3 +4,4 @@ PY_VERSION=`python3 --version`
 echo $PY_VERSION
 pip3 install -r ./requirements.txt
 SKIP_CLEANUP="1" python3 ./scripts/dev/e2e.py --skip-operator-install 1 --skip-image-build 1 --test ${test:?} --tag ${version_id:?} --config_file ./scripts/ci/config.json
+tar -czvf ./logs/e2e/logs.tar.gz ./logs/e2e/*
