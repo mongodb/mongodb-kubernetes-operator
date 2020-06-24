b69c81107eabd7a4dcec6d8f1862ac60d8f6d9a1
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-06-24 09:11:28 +0100

    used individual dockerignore for python formatting build

--- scripts/ci/run_black_linting_check_on_docker.sh
+++ scripts/ci/run_black_linting_check_on_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker --version
-python scripts/dev/dockerfile_generator.py "python_formatting" > Dockerfile
-docker build . -f Dockerfile -t "python_formatting:${version_id:?}"
+python scripts/dev/dockerfile_generator.py "python_formatting" > Dockerfile_python_formatting
+DOCKER_BUILDKIT=1 docker build . -f Dockerfile_python_formatting -t "python_formatting:${version_id:?}"
 docker run "python_formatting:${version_id:?}"
