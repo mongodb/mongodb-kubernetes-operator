18bec55f117f2888a07f3964d9031c9480ea75ba
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-06-25 15:14:36 +0100

    wip

---
+++ test/e2e/setup/setup.go
@@ -0,0 +1,34 @@
+package setup
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/mongodb/mongodb-kubernetes-operator/pkg/apis"
+	mdbv1 "github.com/mongodb/mongodb-kubernetes-operator/pkg/apis/mongodb/v1"
+	f "github.com/operator-framework/operator-sdk/pkg/test"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func InitTest(t *testing.T) (*f.Context, bool) {
+	ctx := f.NewContext(t)
+
+	if err := registerTypesWithFramework(&mdbv1.MongoDB{}); err != nil {
+		t.Fatal(err)
+	}
+
+	skip := os.Getenv("SKIP_CLEANUP")
+
+	fmt.Printf("SKIP_CLEANUP: %s\n", skip)
+	return ctx, skip != "True"
+}
+
+func registerTypesWithFramework(newTypes ...runtime.Object) error {
+	for _, newType := range newTypes {
+		if err := f.AddToFrameworkScheme(apis.AddToScheme, newType); err != nil {
+			return fmt.Errorf("failed to add custom resource type %s to framework scheme: %v", newType.GetObjectKind(), err)
+		}
+	}
+	return nil
+}
