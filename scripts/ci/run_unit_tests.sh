3109c1ca4c40b232c589ac632e951d5c45e95482
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-06-30 12:57:21 +0100

    removed unused pip install

--- scripts/ci/run_unit_tests.sh
+++ scripts/ci/run_unit_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 . venv/bin/activate
-pip3 install -r ./requirements.txt
 python scripts/dev/dockerfile_generator.py "unittest" > Dockerfile
 docker build . -f Dockerfile -t "unit-tests:${version_id:?}"
 docker run "unit-tests:${version_id:?}"
