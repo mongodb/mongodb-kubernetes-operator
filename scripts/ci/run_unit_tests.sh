8a445166d4b2fbd094ee09bc33cebd92092fbfac
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-06-30 10:36:34 +0100

    wip

--- scripts/ci/run_unit_tests.sh
+++ scripts/ci/run_unit_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. venv/bin/activate
+pip3 install -r ./requirements.txt
 python scripts/dev/dockerfile_generator.py "unittest" > Dockerfile
 docker build . -f Dockerfile -t "unit-tests:${version_id:?}"
 docker run "unit-tests:${version_id:?}"
