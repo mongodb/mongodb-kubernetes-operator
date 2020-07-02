a1a30af95c46dd7212e106897cb34a67983d7b80
Fabian Lindfors <fabian@flapplabs.se>
2020-07-02 12:52:37 +0200

    Add E2E test for creating a TLS-enabled cluster

---
+++ test/e2e/replica_set_tls/replica_set_tls_test.go
@@ -0,0 +1,47 @@
+package replica_set_tls
+
+import (
+	"testing"
+
+	mdbv1 "github.com/mongodb/mongodb-kubernetes-operator/pkg/apis/mongodb/v1"
+	e2eutil "github.com/mongodb/mongodb-kubernetes-operator/test/e2e"
+	"github.com/mongodb/mongodb-kubernetes-operator/test/e2e/mongodbtests"
+	setup "github.com/mongodb/mongodb-kubernetes-operator/test/e2e/setup"
+	f "github.com/operator-framework/operator-sdk/pkg/test"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+func TestMain(m *testing.M) {
+	f.MainEntry(m)
+}
+
+func TestReplicaSetTLS(t *testing.T) {
+	ctx, shouldCleanup := setup.InitTest(t)
+	if shouldCleanup {
+		defer ctx.Cleanup()
+	}
+
+	mdb := e2eutil.NewTestMongoDB("mdb0")
+	mdb.Spec.Security.TLS = e2eutil.NewTestTLSConfig(false)
+
+	t.Run("Create TLS Resources", mongodbtests.CreateTLSResources(&mdb, ctx))
+	t.Run("Create MongoDB Resource", mongodbtests.CreateMongoDBResource(&mdb, ctx))
+	t.Run("Config Map Was Correctly Created", mongodbtests.AutomationConfigConfigMapExists(&mdb))
+	t.Run("Stateful Set Reaches Ready State", mongodbtests.StatefulSetIsReady(&mdb))
+	t.Run("Stateful Set has OwnerReference", mongodbtests.StatefulSetHasOwnerReference(&mdb,
+		*metav1.NewControllerRef(&mdb, schema.GroupVersionKind{
+			Group:   mdbv1.SchemeGroupVersion.Group,
+			Version: mdbv1.SchemeGroupVersion.Version,
+			Kind:    mdb.Kind,
+		})))
+	t.Run("MongoDB Reaches Running Phase", mongodbtests.MongoDBReachesRunningPhase(&mdb))
+	t.Run("Wait for TLS to be enabled", mongodbtests.WaitForSetting(&mdb, "sslMode", "requireSSL"))
+	t.Run("Test Basic TLS Connectivity", mongodbtests.BasicConnectivityWithTLS(&mdb))
+	t.Run("Test TLS required", mongodbtests.BasicConnectivityTLSRequired(&mdb))
+	t.Run("Test Status Was Updated", mongodbtests.Status(&mdb,
+		mdbv1.MongoDBStatus{
+			MongoURI: mdb.MongoURI(),
+			Phase:    mdbv1.Running,
+		}))
+}
