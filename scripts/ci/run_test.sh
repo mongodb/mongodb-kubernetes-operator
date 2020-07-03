f2ed32f694199c2f9ad7ba13b5e3f2ab552612e2
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-07-03 15:48:49 +0100

    CLOUDP-65826: Add automatic linting to the codebase (#84)

--- scripts/ci/run_test.sh
+++ scripts/ci/run_test.sh
@@ -2,4 +2,4 @@
 
 
 pip3 install -r ./requirements.txt
-python3 ./scripts/dev/e2e.py --test ${test:?} --tag ${version_id:?} --config_file ./scripts/ci/config.json 
+python3 ./scripts/dev/e2e.py --test "${test:?}" --tag "${version_id:?}" --config_file ./scripts/ci/config.json 
