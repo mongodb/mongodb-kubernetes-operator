fb4b4eb42237e333bbfa0b469cbc2a3c2ba2ca10
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-07-07 13:49:20 +0100

    fix

--- scripts/ci/run_black_formatting_check.sh
+++ scripts/ci/run_black_formatting_check.sh
@@ -2,4 +2,5 @@
 
 # shellcheck disable=SC1091
 . venv/bin/activate
-black --check "$(find . -type f -not -path '*venv*' -name '*.py')"
+# shellcheck disable=SC2046
+black --check $(find . -type f -not -path '*venv*' -name '*.py')
