963f2d1d1b52b6bbe33066239867483d4f479919
Cian Hatton <cianhatton@gmail.com>
2020-07-14 18:28:38 +0100

    Remove hard coded name (#103)

--- scripts/dev/k8s_request_data.py
+++ scripts/dev/k8s_request_data.py
@@ -1,10 +1,10 @@
 from kubernetes.client.rest import ApiException
 from kubernetes import client
 
-from typing import Optional
+from typing import Optional, List, Dict
 
 
-def get_crds() -> Optional[dict]:
+def get_crds() -> Optional[Dict]:
     crdv1 = client.ApiextensionsV1beta1Api()
     try:
         crd = crdv1.list_custom_resource_definition(pretty="true")
@@ -14,7 +14,20 @@ def get_crds() -> Optional[dict]:
     return crd.to_dict()
 
 
-def get_persistent_volumes() -> Optional[dict]:
+def get_all_mongodb_namespaced(namespace: str) -> Optional[List]:
+    customv1 = client.CustomObjectsApi()
+    try:
+        return list(
+            customv1.list_namespaced_custom_object(
+                "mongodb.com", "v1", namespace, "mongodb", pretty=True
+            )["items"]
+        )
+    except ApiException as e:
+        print("Exception when calling get_namespaced_custom_object %s\n" % e)
+    return None
+
+
+def get_persistent_volumes() -> Optional[Dict]:
     corev1 = client.CoreV1Api()
     try:
         pv = corev1.list_persistent_volume(pretty="true")
@@ -24,7 +37,7 @@ def get_persistent_volumes() -> Optional[dict]:
     return pv.to_dict()
 
 
-def get_stateful_sets_namespaced(namespace: str) -> Optional[dict]:
+def get_stateful_sets_namespaced(namespace: str) -> Optional[Dict]:
     av1beta1 = client.AppsV1Api()
     try:
         sst = av1beta1.list_namespaced_stateful_set(namespace, pretty="true")
@@ -34,7 +47,7 @@ def get_stateful_sets_namespaced(namespace: str) -> Optional[dict]:
     return sst.to_dict()
 
 
-def get_configmap_namespaced(namespace: str, name: str) -> Optional[dict]:
+def get_configmap_namespaced(namespace: str, name: str) -> Optional[Dict]:
     corev1 = client.CoreV1Api()
     try:
         config_map = corev1.read_namespaced_config_map(name, namespace, pretty="true")
@@ -44,7 +57,7 @@ def get_configmap_namespaced(namespace: str, name: str) -> Optional[dict]:
     return config_map.to_dict()
 
 
-def get_pods_namespaced(namespace: str) -> Optional[list]:
+def get_pods_namespaced(namespace: str) -> Optional[List]:
     corev1 = client.CoreV1Api()
     try:
         pods = corev1.list_namespaced_pod(namespace)
@@ -60,6 +73,7 @@ def get_pod_namespaced(namespace: str, pod_name: str) -> Optional[client.V1Pod]:
         pod = corev1.read_namespaced_pod(name=pod_name, namespace=namespace)
     except ApiException as e:
         print("Exception when calling read_namespaced_pod: %s\n" % e)
+        return None
     return pod
 
 
