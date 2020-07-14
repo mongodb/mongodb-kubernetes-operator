b8ec60fab95d380e7a607670a8f95d95119971d5
chatton <cianhatton@gmail.com>
2020-07-14 13:58:57 +0100

    added password to tls calls

--- test/e2e/replica_set_tls/replica_set_tls_test.go
+++ test/e2e/replica_set_tls/replica_set_tls_test.go
@@ -36,6 +36,6 @@ func TestReplicaSetTLS(t *testing.T) {
 	t.Run("Create MongoDB Resource", mongodbtests.CreateMongoDBResource(&mdb, ctx))
 	t.Run("Basic tests", mongodbtests.BasicFunctionality(&mdb))
 	t.Run("Wait for TLS to be enabled", tlstests.WaitForTLSMode(&mdb, "requireSSL", user.Name, password))
-	t.Run("Test Basic TLS Connectivity", tlstests.ConnectivityWithTLS(&mdb))
+	t.Run("Test Basic TLS Connectivity", tlstests.ConnectivityWithTLS(&mdb, user.Name, password))
 	t.Run("Test TLS required", tlstests.ConnectivityWithoutTLSShouldFail(&mdb, user.Name, password))
 }
