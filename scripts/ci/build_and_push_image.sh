c7609c05d33461c5573ff3e893c2b8e5a96b648a
chatton <cianhatton@gmail.com>
2020-07-17 13:16:17 +0100

    label directly in docker build

--- scripts/ci/build_and_push_image.sh
+++ scripts/ci/build_and_push_image.sh
@@ -6,5 +6,7 @@
 echo "${quay_password:?}" | docker login "-u=${quay_user_name:?}" quay.io --password-stdin
 
 python3 scripts/dev/dockerfile_generator.py "${image_type:?}" > Dockerfile
-docker build . --build-arg quay_expiration="${expire_after:-never}" -f Dockerfile -t "${image:?}"
+
+# Providing the quay.expires-after configures quay to delete this image after the provided amount of time
+docker build . --label "quay.expires-after=${expire_after:-never}" -f Dockerfile -t "${image:?}"
 docker push "${image:?}"
