520833b15f614e27ea68a5c4d938849b73df055e
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-07-21 18:09:33 +0100

    removed unused code

--- test/e2e/statefulset_arbitrary_config/statefulset_arbitrary_config_test.go
+++ test/e2e/statefulset_arbitrary_config/statefulset_arbitrary_config_test.go
@@ -1,16 +1,14 @@
 package statefulset_arbitrary_config
 
 import (
-	"context"
 	"testing"
 
-	"github.com/golangplus/testing/assert"
 	v1 "github.com/mongodb/mongodb-kubernetes-operator/pkg/apis/mongodb/v1"
 	e2eutil "github.com/mongodb/mongodb-kubernetes-operator/test/e2e"
 	"github.com/mongodb/mongodb-kubernetes-operator/test/e2e/mongodbtests"
 	setup "github.com/mongodb/mongodb-kubernetes-operator/test/e2e/setup"
 	f "github.com/operator-framework/operator-sdk/pkg/test"
-	appsv1 "k8s.io/api/apps/v1"
+	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -36,17 +34,13 @@ func TestStatefulSetArbitraryConfig(t *testing.T) {
 	t.Run("Test Basic Connectivity", mongodbtests.Connectivity(&mdb))
 	t.Run("AutomationConfig has the correct version", mongodbtests.AutomationConfigVersionHasTheExpectedVersion(&mdb, 1))
 
-	// Get the original containers
-	originalSts := &appsv1.StatefulSet{}
-	err = f.Global.Client.Get(context.TODO(), mdb.NamespacedName(), originalSts)
-	assert.NoError(t, err)
-
 	overrideSpec := v1.StatefulSetConfiguration{}
 	overrideSpec.Spec.Template.Spec.Containers = []corev1.Container{
 		{Name: "mongodb-agent", ReadinessProbe: &corev1.Probe{TimeoutSeconds: 100}}}
 
-	e2eutil.UpdateMongoDBResource(&mdb, func(mdb *v1.MongoDB) { mdb.Spec.StatefulSetConfiguration = overrideSpec })
+	err = e2eutil.UpdateMongoDBResource(&mdb, func(mdb *v1.MongoDB) { mdb.Spec.StatefulSetConfiguration = overrideSpec })
+	assert.NoError(t, err)
 
-	t.Run("Container has been merged by name", mongodbtests.StatefulSetContainerConditionIsTrue(mdb, "mongodb-agent", func(container corev1.Container) { return container.ReadinessProbe.TimeoutSeconds == 100 }))
+	t.Run("Container has been merged by name", mongodbtests.StatefulSetContainerConditionIsTrue(&mdb, "mongodb-agent", func(container corev1.Container) bool { return container.ReadinessProbe.TimeoutSeconds == 100 }))
 
 }
