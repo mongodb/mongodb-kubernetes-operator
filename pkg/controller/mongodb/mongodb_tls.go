f4a65ba56fbe027f5bd788f397609c351b35908f
chatton <cianhatton@gmail.com>
2020-07-22 10:26:38 +0100

    added nil check on MDB annotations

--- pkg/controller/mongodb/mongodb_tls.go
+++ pkg/controller/mongodb/mongodb_tls.go
@@ -121,13 +121,14 @@ func (r *ReplicaSetReconciler) completeTLSRollout(mdb mdbv1.MongoDB) error {
 	}
 
 	r.log.Debug("Completing TLS rollout")
+	if mdb.Annotations == nil {
+		mdb.Annotations = map[string]string{}
+	}
+	mdb.Annotations[tLSRolledOutAnnotationKey] = trueAnnotation
 	if err := r.ensureAutomationConfig(mdb); err != nil {
 		return fmt.Errorf("error updating automation config after TLS rollout: %+v", err)
 	}
-	mdbAnnotations := map[string]string{
-		tLSRolledOutAnnotationKey: trueAnnotation,
-	}
-	if err := r.setAnnotations(mdb.NamespacedName(), mdbAnnotations); err != nil {
+	if err := r.setAnnotations(mdb.NamespacedName(), mdb.Annotations); err != nil {
 		return fmt.Errorf("error setting TLS annotation: %+v", err)
 	}
 
