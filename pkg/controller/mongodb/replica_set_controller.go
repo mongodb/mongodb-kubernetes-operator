43bf744d8f34f418ddbb32aed6ba0ff282986fa2
Fabian Lindfors <fabian@flapplabs.se>
2020-07-29 13:41:15 +0200

    Add custom specs during statefulset creation

--- pkg/controller/mongodb/replica_set_controller.go
+++ pkg/controller/mongodb/replica_set_controller.go
@@ -605,6 +605,7 @@ func buildStatefulSetModificationFunction(mdb mdbv1.MongoDB) statefulset.Modific
 				buildScramPodSpecModification(mdb),
 			),
 		),
+		statefulset.WithCustomSpecs(mdb.Spec.StatefulSetConfiguration.Spec),
 	)
 }
 
