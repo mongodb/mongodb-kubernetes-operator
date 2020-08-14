29d865cac1fdcf1697d2f262b60830f37d09c53b
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-08-14 15:28:17 +0100

    CLOUDP-68700: test tag

--- scripts/ci/release_image.py
+++ scripts/ci/release_image.py
@@ -3,23 +3,34 @@ import argparse
 
 sys.path.append("./scripts/dev/")
 import dockerutil
+import docker
 import json
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("repo_url", help="Url of the image to retag", type=str)
+    parser.add_argument("old_repo_url", help="Url of the image to retag", type=str)
+    parser.add_argument(
+        "new_repo_url", help="Url to where the new image should be pushed", type=str
+    )
     parser.add_argument(
         "--path",
         help="path to use for the temporarily generated Dockerfile",
         type=str,
         default=".",
     )
+    parser.add_argument("release_file", help="Path to the release file", type=str)
     parser.add_argument(
         "old_tag", help="the old tag of the image to retag", type=str,
     )
     parser.add_argument(
-        "new_tag", help="the new tag of the image to retag", type=str,
+        "username", help="username for the registry", type=str,
+    )
+    parser.add_argument(
+        "password", help="password for the registry", type=str,
+    )
+    parser.add_argument(
+        "registry", help="The docker registry", type=str,
     )
     parser.add_argument(
         "--labels", help="Labels for the new image", type=json.loads,
@@ -31,8 +42,19 @@ def parse_args() -> argparse.Namespace:
 
 def main() -> int:
     args = parse_args()
+    with open(args.release_file) as f:
+        release = json.load(f)
+
+    new_tag = release["mongodb-kubernetes-operator"]
     dockerutil.retag_image(
-        args.repo_url, args.old_tag, args.new_tag, args.path, args.labels
+        args.repo_url,
+        args.old_tag,
+        new_tag,
+        args.path,
+        args.labels,
+        args.username,
+        args.password,
+        args.registry,
     )
     return 0
 
