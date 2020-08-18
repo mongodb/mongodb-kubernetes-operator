ffd7897f3c0f185e953d201b7a4c59c5ac0186ff
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-08-18 18:15:16 +0200

    CLOUDP-68700: automatic release process (#141)

--- scripts/dev/dockerutil.py
+++ scripts/dev/dockerutil.py
@@ -3,7 +3,7 @@ from dockerfile_generator import render
 import os
 import json
 
-from typing import Union, Any
+from typing import Union, Any, Optional
 
 
 def build_image(repo_url: str, tag: str, path: str) -> None:
@@ -26,6 +26,42 @@ def push_image(tag: str) -> None:
     progress = ""
     for line in client.images.push(tag, stream=True):
         print("\r" + push_image_formatted(line), end="", flush=True)
+
+
+def retag_image(
+    old_repo_url: str,
+    new_repo_url: str,
+    old_tag: str,
+    new_tag: str,
+    path: str,
+    labels: Optional[dict] = None,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+    registry: Optional[str] = None,
+) -> None:
+    with open(f"{path}/Dockerfile", "w") as f:
+        f.write(f"FROM {old_repo_url}:{old_tag}")
+    client = docker.from_env()
+    if all(value is not None for value in [username, password, registry]):
+        client.login(username=username, password=password, registry=registry)
+
+    image, _ = client.images.build(path=f"{path}", labels=labels, tag=new_tag)
+    image.tag(new_repo_url, new_tag)
+    os.remove(f"{path}/Dockerfile")
+
+    # We do not want to republish an image that has not changed, so we check if the new
+    # pair repo:tag already exists.
+    try:
+        image = client.images.pull(new_repo_url, new_tag)
+        return
+    # We also need to catch APIError as if the image has been recently deleted (uncommon, but might happen?)
+    # we will get this kind of error:
+    # docker.errors.APIError: 500 Server Error: Internal Server Error
+    # ("unknown: Tag <tag> was deleted or has expired. To pull, revive via time machine"
+    except (docker.errors.ImageNotFound, docker.errors.APIError) as e:
+        pass
+    print(f"Pushing to {new_repo_url}:{new_tag}")
+    client.images.push(new_repo_url, new_tag)
 
 
 def push_image_formatted(line: Any) -> str:
