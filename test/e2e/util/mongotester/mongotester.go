e355a95f44d38dafb8dade4e432189f17ca40a43
Cian Hatton <cianhatton@gmail.com>
2020-09-02 16:55:17 +0100

    TLS support for mongo tester (#166)

--- test/e2e/util/mongotester/mongotester.go
+++ test/e2e/util/mongotester/mongotester.go
@@ -2,7 +2,10 @@ package mongotester
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"reflect"
 	"testing"
 	"time"
@@ -18,25 +21,52 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
+// tlsConfig is the test tls fixture that we use for testing
+// tls.
+var tlsConfig *tls.Config = nil
+
+// initTls loads in the tls configuration fixture
+func initTls() error {
+	if tlsConfig != nil {
+		return nil
+	}
+	var err error
+	tlsConfig, err = getClientTLSConfig()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 type Tester struct {
 	mongoClient *mongo.Client
 	clientOpts  []*options.ClientOptions
 }
 
-func NewTester(opts ...*options.ClientOptions) (*Tester, error) {
+func newTester(opts ...*options.ClientOptions) (*Tester, error) {
+	if err := initTls(); err != nil {
+		return nil, err
+	}
+
 	t := &Tester{}
 	t.clientOpts = append(t.clientOpts, opts...)
 	return t, nil
 }
 
+// OptionApplier is an interface which is able to accept a list
+// of options.ClientOptions, and return the final desired list
+// making any modifications required
+type OptionApplier interface {
+	ApplyOption(opts ...*options.ClientOptions) []*options.ClientOptions
+}
+
 // FromResource returns a Tester instance from a MongoDB resource. It infers SCRAM username/password
 // and the hosts from the resource.
-// NOTE: Tls is not configured as the mechanism that the ClientOptions are merged only merge on non-nil
-// values, meaning we need to remove option that configures TLS from the list if we want to not use tls.
-// For now we can just explicitly pass WithTls() or WithoutTls() to configure TLS.
-func FromResource(t *testing.T, mdb mdbv1.MongoDB, opts ...*options.ClientOptions) (*Tester, error) {
+func FromResource(t *testing.T, mdb mdbv1.MongoDB, opts ...OptionApplier) (*Tester, error) {
 	var clientOpts []*options.ClientOptions
-	clientOpts = append(clientOpts, WithHosts(mdb.Hosts()))
+
+	clientOpts = WithHosts(mdb.Hosts()).ApplyOption(clientOpts...)
+
 	t.Logf("Configuring hosts: %s for MongoDB: %s", mdb.Hosts(), mdb.NamespacedName())
 
 	users := mdb.Spec.Users
@@ -48,24 +78,30 @@ func FromResource(t *testing.T, mdb mdbv1.MongoDB, opts ...*options.ClientOption
 			return nil, err
 		}
 		t.Logf("Configuring SCRAM username: %s and password from secret %s for MongoDB: %s", user.Name, user.PasswordSecretRef.Name, mdb.NamespacedName())
-		clientOpts = append(clientOpts, WithScram(user.Name, string(passwordSecret.Data[user.PasswordSecretRef.Key])))
+		clientOpts = WithScram(user.Name, string(passwordSecret.Data[user.PasswordSecretRef.Key])).ApplyOption(clientOpts...)
+	}
+
+	if mdb.Spec.Security.TLS.Enabled {
+		clientOpts = WithTls().ApplyOption(clientOpts...)
 	}
 
 	// add any additional options
-	clientOpts = append(clientOpts, opts...)
+	for _, opt := range opts {
+		clientOpts = opt.ApplyOption(clientOpts...)
+	}
 
-	return NewTester(clientOpts...)
+	return newTester(clientOpts...)
 }
 
 // ConnectivitySucceeds performs a basic check that ensures that it is possible
 // to connect to the MongoDB resource
-func (m *Tester) ConnectivitySucceeds(opts ...*options.ClientOptions) func(t *testing.T) {
+func (m *Tester) ConnectivitySucceeds(opts ...OptionApplier) func(t *testing.T) {
 	return m.connectivityCheck(true, opts...)
 }
 
 // ConnectivitySucceeds performs a basic check that ensures that it is not possible
 // to connect to the MongoDB resource
-func (m *Tester) ConnectivityFails(opts ...*options.ClientOptions) func(t *testing.T) {
+func (m *Tester) ConnectivityFails(opts ...OptionApplier) func(t *testing.T) {
 	return m.connectivityCheck(false, opts...)
 }
 
@@ -75,6 +111,10 @@ func (m *Tester) HasKeyfileAuth(tries int) func(t *testing.T) {
 
 func (m *Tester) HasFCV(fcv string, tries int) func(t *testing.T) {
 	return m.hasAdminParameter("featureCompatibilityVersion", map[string]interface{}{"version": fcv}, tries)
+}
+
+func (m *Tester) HasTlsMode(tlsMode string, tries int) func(t *testing.T) {
+	return m.hasAdminParameter("sslMode", tlsMode, tries)
 }
 
 func (m *Tester) hasAdminParameter(key string, expectedValue interface{}, tries int) func(t *testing.T) {
@@ -90,7 +130,7 @@ func (m *Tester) hasAdminParameter(key string, expectedValue interface{}, tries 
 		for !found && tries > 0 {
 			<-time.After(10 * time.Second)
 			actualValue, err := m.getAdminSetting(key)
-			t.Logf("actualValue: %+v", actualValue)
+			t.Logf("Actual Value: %+v", actualValue)
 			if err != nil {
 				t.Logf("Unable to get admin setting %s with error : %s", key, err)
 				continue
@@ -116,13 +156,19 @@ func (m *Tester) getAdminSetting(key string) (interface{}, error) {
 	return value, nil
 }
 
-func (m *Tester) connectivityCheck(shouldSucceed bool, opts ...*options.ClientOptions) func(t *testing.T) {
+func (m *Tester) connectivityCheck(shouldSucceed bool, opts ...OptionApplier) func(t *testing.T) {
+
+	clientOpts := make([]*options.ClientOptions, 0)
+	for _, optApplier := range opts {
+		clientOpts = optApplier.ApplyOption(clientOpts...)
+	}
+
 	connectivityOpts := defaults()
 	return func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), connectivityOpts.ContextTimeout)
 		defer cancel()
 
-		if err := m.ensureClient(opts...); err != nil {
+		if err := m.ensureClient(clientOpts...); err != nil {
 			t.Fatal(err)
 		}
 
@@ -152,7 +198,7 @@ func (m *Tester) connectivityCheck(shouldSucceed bool, opts ...*options.ClientOp
 
 // StartBackgroundConnectivityTest starts periodically checking connectivity to the MongoDB deployment
 // with the defined interval. A cancel function is returned, which can be called to stop testing connectivity.
-func (m *Tester) StartBackgroundConnectivityTest(t *testing.T, interval time.Duration, opts ...*options.ClientOptions) func() {
+func (m *Tester) StartBackgroundConnectivityTest(t *testing.T, interval time.Duration, opts ...OptionApplier) func() {
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Logf("Starting background connectivity test")
 
@@ -189,24 +235,98 @@ func (t *Tester) ensureClient(opts ...*options.ClientOptions) error {
 	return nil
 }
 
+// clientOptionAdder is the standard implementation that simply adds a
+// new options.ClientOption to the mongo client
+type clientOptionAdder struct {
+	option *options.ClientOptions
+}
+
+func (c clientOptionAdder) ApplyOption(opts ...*options.ClientOptions) []*options.ClientOptions {
+	return append(opts, c.option)
+}
+
+// clientOptionRemover is used if a value from the client array of options should be removed.
+// assigning a nil value will not take precedence over an existing value, so we need a mechanism
+// to remove elements that are present
+
+// e.g. to disable TLS, you need to remove the options.ClientOption that has a non-nil tls config
+// it is not enough to add a tls config that has a nil value.
+type clientOptionRemover struct {
+	// removalPredicate is a function which returns a bool indicating
+	// if a given options.ClientOption should be removed.
+	removalPredicate func(opt *options.ClientOptions) bool
+}
+
+func (c clientOptionRemover) ApplyOption(opts ...*options.ClientOptions) []*options.ClientOptions {
+	newOpts := make([]*options.ClientOptions, 0)
+	for _, opt := range opts {
+		if !c.removalPredicate(opt) {
+			newOpts = append(newOpts, opt)
+		}
+	}
+	return newOpts
+}
+
 // WithScram provides a configuration option that will configure the MongoDB resource
 // with the given username and password
-func WithScram(username, password string) *options.ClientOptions {
-	return &options.ClientOptions{
-		Auth: &options.Credential{
-			AuthMechanism: "SCRAM-SHA-256",
-			AuthSource:    "admin",
-			Username:      username,
-			Password:      password,
+func WithScram(username, password string) OptionApplier {
+	return clientOptionAdder{
+		option: &options.ClientOptions{
+			Auth: &options.Credential{
+				AuthMechanism: "SCRAM-SHA-256",
+				AuthSource:    "admin",
+				Username:      username,
+				Password:      password,
+			},
 		},
 	}
 }
 
 // WithHosts configures the hosts of the deployment
-func WithHosts(hosts []string) *options.ClientOptions {
-	return &options.ClientOptions{
-		Hosts: hosts,
+func WithHosts(hosts []string) OptionApplier {
+	return clientOptionAdder{
+		option: &options.ClientOptions{
+			Hosts: hosts,
+		},
 	}
+}
+
+// WithTls configures the client to use tls
+func WithTls() OptionApplier {
+	return withTls(tlsConfig)
+}
+
+func withTls(tls *tls.Config) OptionApplier {
+	return clientOptionAdder{
+		option: &options.ClientOptions{
+			TLSConfig: tls,
+		},
+	}
+}
+
+// WithoutTls will remove the tls configuration
+func WithoutTls() OptionApplier {
+	return clientOptionRemover{
+		removalPredicate: func(opt *options.ClientOptions) bool {
+			return opt.TLSConfig != nil
+		},
+	}
+}
+
+// getClientTLSConfig reads in the tls fixtures
+func getClientTLSConfig() (*tls.Config, error) {
+	// Read the CA certificate from test data
+	caPEM, err := ioutil.ReadFile("testdata/tls/ca.crt")
+	if err != nil {
+		return nil, err
+	}
+
+	caPool := x509.NewCertPool()
+	caPool.AppendCertsFromPEM(caPEM)
+
+	return &tls.Config{
+		RootCAs: caPool,
+	}, nil
 }
 
 // defaults returns the default connectivity options
