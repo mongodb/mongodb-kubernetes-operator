1d75af0116c14dadc5106f42d183d284a9cd04f5
chatton <cianhatton@gmail.com>
2020-09-09 11:13:19 +0100

    Release Community Operator v0.2.1

--- cmd/testrunner/main.go
+++ cmd/testrunner/main.go
@@ -21,7 +21,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
-
 	apiErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
