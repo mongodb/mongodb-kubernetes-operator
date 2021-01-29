5f2ac62d8f413198d9b4cd3ef38f41403da63149
chatton <cianhatton@gmail.com>
2021-01-29 13:43:02 +0000

    Default to empty struct for additional mongodconfig

--- test/e2e/e2eutil.go
+++ test/e2e/e2eutil.go
@@ -154,6 +154,7 @@ func NewTestMongoDB(name string, namespace string) (mdbv1.MongoDBCommunity, mdbv
 					Modes: []mdbv1.AuthMode{"SCRAM"},
 				},
 			},
+			AdditionalMongodConfig: mdbv1.MongodConfiguration{},
 			Users: []mdbv1.MongoDBUser{
 				{
 					Name: fmt.Sprintf("%s-user", name),
