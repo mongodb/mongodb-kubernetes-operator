99e15e55d07259f992b9315fe59cd22c40158667
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2021-02-05 11:13:23 +0100

    Move black formatting to GitHub action (#322)

--- scripts/ci/run_mypy_check.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC1091
-. venv/bin/activate
-find . -type f -not -path '*venv*' -name '*.py' -exec python3 -m mypy --disallow-untyped-calls --disallow-untyped-defs --disallow-incomplete-defs --ignore-missing-imports {} +
