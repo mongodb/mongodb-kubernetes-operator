f89e059d32aa2ec95bb5ba7ebed798cb0185fee9
chatton <cianhatton@gmail.com>
2021-04-19 13:18:46 +0100

    configure to use config env va

--- scripts/ci/setup_virtualenv.sh
+++ scripts/ci/setup_virtualenv.sh
@@ -5,6 +5,4 @@ virtualenv --python /opt/python/3.7/bin/python3 ./venv
 . venv/bin/activate
 
 pip3 install -r ./requirements.txt
-
-# shellcheck disable=SC2154
 pip3 install "git+https://${sonar_github_token}@github.com/10gen/sonar.git@0.0.8"
