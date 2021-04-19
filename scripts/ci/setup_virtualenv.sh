334d6e55e21cb10f78eed2df7488a5b3c2bd2b9f
chatton <cianhatton@gmail.com>
2021-04-19 12:17:47 +0100

    test

--- scripts/ci/setup_virtualenv.sh
+++ scripts/ci/setup_virtualenv.sh
@@ -3,5 +3,10 @@
 # shellcheck disable=SC1091
 virtualenv --python /opt/python/3.7/bin/python3 ./venv
 . venv/bin/activate
+
+
+echo "TEST"
+git config user.name
+
 pip3 install -r ./requirements.txt
 pip3 install git+https://${SONAR_GITHUB_TOKEN}@github.com/10gen/sonar.git@0.0.8
