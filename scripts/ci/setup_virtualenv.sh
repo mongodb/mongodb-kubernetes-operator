535b15e31aeec280c5cc687b487951786a57e718
chatton <cianhatton@gmail.com>
2021-04-19 12:27:23 +0100

    add expansion in env

--- scripts/ci/setup_virtualenv.sh
+++ scripts/ci/setup_virtualenv.sh
@@ -5,4 +5,4 @@ virtualenv --python /opt/python/3.7/bin/python3 ./venv
 . venv/bin/activate
 
 pip3 install -r ./requirements.txt
-echo echo "chatton" | pip3 install git+https://${SONAR_GITHUB_TOKEN}@github.com/10gen/sonar.git@0.0.8
+pip3 install git+https://${SONAR_GITHUB_TOKEN}@github.com/10gen/sonar.git@0.0.8
