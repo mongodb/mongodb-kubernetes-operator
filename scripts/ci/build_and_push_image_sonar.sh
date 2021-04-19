1eaa170fafe2fd1dfdd03a334a40639845d1b9b7
chatton <cianhatton@gmail.com>
2021-04-19 13:05:47 +0100

    fix shell check errors

--- scripts/ci/build_and_push_image_sonar.sh
+++ scripts/ci/build_and_push_image_sonar.sh
@@ -5,4 +5,5 @@
 . venv/bin/activate
 echo "${quay_password:?}" | docker login "-u=${quay_user_name:?}" quay.io --password-stdin
 
+# shellcheck disable=SC2154
 python3 pipeline.py --image-name "${image_name}"
