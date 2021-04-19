479217d4202986d0c38257c72c64d0ce377078af
chatton <cianhatton@gmail.com>
2021-04-19 10:51:22 +0100

    updated github token

--- scripts/ci/setup_virtualenv.sh
+++ scripts/ci/setup_virtualenv.sh
@@ -4,3 +4,4 @@
 virtualenv --python /opt/python/3.7/bin/python3 ./venv
 . venv/bin/activate
 pip3 install -r ./requirements.txt
+pip3 install git+https://${SONAR_GITHUB_TOKEN}@github.com/10gen/sonar.git@0.0.8
