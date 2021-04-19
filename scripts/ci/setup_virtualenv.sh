e1490d584a6765737098c0208f933d39e1de3050
chatton <cianhatton@gmail.com>
2021-04-19 13:08:10 +0100

    fix quotes

--- scripts/ci/setup_virtualenv.sh
+++ scripts/ci/setup_virtualenv.sh
@@ -7,4 +7,5 @@ virtualenv --python /opt/python/3.7/bin/python3 ./venv
 pip3 install -r ./requirements.txt
 
 # shellcheck disable=SC2154
-pip3 install -r "git+https://${sonar_github_token}@github.com/10gen/sonar.git@0.0.8"
+#pip3 install -r "git+https://${sonar_github_token}@github.com/10gen/sonar.git@0.0.8"
+pip3 install -r git+https://${sonar_github_token}@github.com/10gen/sonar.git@0.0.8
