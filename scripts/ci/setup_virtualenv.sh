9038ebdffcd26e1950b68e86caa59a8d198d5544
Cian Hatton <cianhatton@gmail.com>
2021-04-20 16:06:34 +0100

    CLOUDP-83092: Add sonar inventory (#422)

--- scripts/ci/setup_virtualenv.sh
+++ scripts/ci/setup_virtualenv.sh
@@ -3,4 +3,8 @@
 # shellcheck disable=SC1091
 virtualenv --python /opt/python/3.7/bin/python3 ./venv
 . venv/bin/activate
+
 pip3 install -r ./requirements.txt
+
+# shellcheck disable=SC2154
+pip3 install "git+https://${sonar_github_token}@github.com/10gen/sonar.git@0.0.9"
