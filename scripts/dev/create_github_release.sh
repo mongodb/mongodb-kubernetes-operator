35d73730e538b08d131542a204344aa199814b4a
chatton <cianhatton@gmail.com>
2021-04-23 10:57:04 +0100

    fixed create_github_release script

--- scripts/dev/create_github_release.sh
+++ scripts/dev/create_github_release.sh
@@ -4,4 +4,4 @@ version="$(jq -r '."mongodb-kubernetes-operator"' < ./release.json)"
 gh release create v"${version}" --title "MongoDB Kubernetes Operator ${version}" --draft --notes-file ./dev_notes/RELEASE_NOTES.md
 
 # move the release notes
-cp ./dev_notes/RELEASE_NOTES.md "./dev_notes/past_release_notes/$v{version}.md"
+cp ./dev_notes/RELEASE_NOTES.md "./dev_notes/past_release_notes/v${version}.md"
