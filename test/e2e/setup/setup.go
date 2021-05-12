38dd42cb3dd734d45bcecaabcaaa9720a9adf0f8
chatton <cianhatton@gmail.com>
2021-05-12 16:52:11 +0100

    corrected env var

--- test/e2e/setup/setup.go
+++ test/e2e/setup/setup.go
@@ -147,7 +147,7 @@ func deployOperator() error {
 		withVersionUpgradeHookImage(testConfig.versionUpgradeHookImage),
 		withEnvVar("WATCH_NAMESPACE", watchNamespace),
 		withEnvVar(construct.AgentImageEnv, testConfig.agentImage),
-		withEnvVar(construct.ReadinessProbeContainerName, testConfig.readinessProbeImage),
+		withEnvVar(construct.ReadinessProbeImageEnv, testConfig.readinessProbeImage),
 	); err != nil {
 		return errors.Errorf("error building operator deployment: %s", err)
 	}
