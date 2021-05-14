19a911b69030657e479bc6a55bbe19b07e2a0015
Caio Ferreira <caiorcferreira@gmail.com>
2021-05-14 04:56:27 -0300

    Fix pod annotations overwrite (#484)

--- cmd/readiness/testdata/k8sobjects.go
+++ cmd/readiness/testdata/k8sobjects.go
@@ -7,6 +7,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// Currently seems like the appending functionality on the library used by the fake
+// implementation to simulate JSONPatch is broken: https://github.com/evanphx/json-patch/issues/138
+// The short term workaround is to have the annotation empty.
+
 // These are just k8s objects used for testing. Note, that these are defined in a non "_test.go" file as they are reused
 // by other modules
 func TestSecret(namespace, name string, version int) *corev1.Secret {
@@ -21,6 +25,9 @@ func TestPod(namespace, name string) *corev1.Pod {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
+			Annotations: map[string]string{
+				"agent.mongodb.com/version": "",
+			},
 		},
 	}
 }
