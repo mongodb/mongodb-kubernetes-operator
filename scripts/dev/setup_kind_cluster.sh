9a12f9d86065826dd92af9eb67d22389cc669b3a
antonlisovenko <anton.lisovenko@mongodb.com>
2021-05-24 12:39:42 +0100

    kind cluster script

--- scripts/dev/setup_kind_cluster.sh
+++ scripts/dev/setup_kind_cluster.sh
@@ -29,3 +29,17 @@ containerdConfigPatches:
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
     endpoint = ["http://${ip}:${reg_port}"]
 EOF
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
