e47c98ba809397a7c7a0131c7a04140774be1ca0
chatton <cianhatton@gmail.com>
2021-05-25 18:23:25 +0100

    Removed unused python scripts and converted evergreen task to use pipeline

--- scripts/ci/run_image_release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC1091
-. venv/bin/activate
-python3 scripts/dev/release_image.py --old_repo_url "${old_image:-}" --new_repo_url "${new_image:-}" --old_tag "${version_id:-}" --labels "{\"quay.expires-after\":\"never\"}" --username "${quay_user_name:-}" --password "${quay_password:-}" --registry https://quay.io/organization/mongodb --release_file "./release.json" --image_type "${image_type:-}"
