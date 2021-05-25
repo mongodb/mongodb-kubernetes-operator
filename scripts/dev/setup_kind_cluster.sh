4c24df6bb5f1137a143102ac9d5fc0983bdcf4a8
antonlisovenko <anton.lisovenko@mongodb.com>
2021-05-25 08:50:16 +0100

    pr comments

--- scripts/dev/setup_kind_cluster.sh
+++ scripts/dev/setup_kind_cluster.sh
@@ -30,7 +30,7 @@ containerdConfigPatches:
     endpoint = ["http://${ip}:${reg_port}"]
 EOF
 
-# Document the local registry
+# Document the local registry (from  https://kind.sigs.k8s.io/docs/user/local-registry/)
 # https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
