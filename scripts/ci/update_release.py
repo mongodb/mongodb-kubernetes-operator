d51820ac0fe10196eb86194b3b5627dc08c6b3e1
chatton <cianhatton@gmail.com>
2021-06-02 21:05:34 +0100

    fixed mypy errors

--- scripts/ci/update_release.py
+++ scripts/ci/update_release.py
@@ -9,17 +9,17 @@ RELATIVE_PATH_TO_MANAGER_YAML = "config/manager/manager.yaml"
 RELATIVE_PATH_TO_OPENSHIFT_MANAGER_YAML = "deploy/openshift/operator_openshift.yaml"
 
 
-def _load_yaml_file(path):
+def _load_yaml_file(path: str) -> Dict:
     with open(path, "r") as f:
         return yaml.safe_load(f.read())
 
 
-def _dump_yaml(operator: Dict, path: str):
+def _dump_yaml(operator: Dict, path: str) -> None:
     with open(path, "w+") as f:
         return yaml.dump(operator, f)
 
 
-def update_and_write_file(path: str):
+def update_and_write_file(path: str) -> None:
     release = _load_release()
     yaml_file = _load_yaml_file(path)
     _update_operator_deployment(yaml_file, release)
@@ -37,8 +37,12 @@ def _replace_tag(image: str, new_tag: str) -> str:
 
 
 def _update_operator_deployment(operator_deployment: Dict, release: Dict) -> None:
-    operator_container = operator_deployment["spec"]["template"]["spec"]["containers"][0]
-    operator_container["image"] = _replace_tag(operator_container["image"], release["mongodb-kubernetes-operator"])
+    operator_container = operator_deployment["spec"]["template"]["spec"]["containers"][
+        0
+    ]
+    operator_container["image"] = _replace_tag(
+        operator_container["image"], release["mongodb-kubernetes-operator"]
+    )
     operator_envs = operator_container["env"]
     for env in operator_envs:
         if env["name"] == "VERSION_UPGRADE_HOOK_IMAGE":
@@ -46,7 +50,9 @@ def _update_operator_deployment(operator_deployment: Dict, release: Dict) -> Non
         if env["name"] == "READINESS_PROBE_IMAGE":
             env["value"] = _replace_tag(env["value"], release["readiness-probe"])
         if env["name"] == "AGENT_IMAGE":
-            env["value"] = _replace_tag(env["value"], release["mongodb-agent"]["version"])
+            env["value"] = _replace_tag(
+                env["value"], release["mongodb-agent"]["version"]
+            )
 
 
 def main() -> int:
