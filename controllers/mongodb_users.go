e315644694f0947eced4aa285b711934d91b3d65
Ciprian Tibulca <ciprian.tibulca@mongodb.com>
2021-06-02 15:04:25 +0100

    expose connection strings - add docstring

--- controllers/mongodb_users.go
+++ controllers/mongodb_users.go
@@ -9,9 +9,11 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
+// ensureUserResources will check that the configured user password secrets can be found
+// and will start monitor them so that the reconcile process is triggered every time these secrets are updated
 func (r ReplicaSetReconciler) ensureUserResources(mdb mdbv1.MongoDBCommunity) error {
 	for _, user := range mdb.GetScramUsers() {
-		secretNamespacedName := types.NamespacedName{Name: user.PasswordSecretName, Namespace: mdb.NamespacedName().Namespace}
+		secretNamespacedName := types.NamespacedName{Name: user.PasswordSecretName, Namespace: mdb.Namespace}
 		if _, err := secret.ReadKey(r.client, user.PasswordSecretKey, secretNamespacedName); err != nil {
 			if apiErrors.IsNotFound(err) {
 				r.log.Errorf(`User password secret "%s" not found`, secretNamespacedName)
@@ -25,9 +27,11 @@ func (r ReplicaSetReconciler) ensureUserResources(mdb mdbv1.MongoDBCommunity) er
 	return nil
 }
 
+// updateConnectionStringSecrets updates secrets where user specific connection strings are stored.
+// The client applications can mount these secrets and connect to the mongodb cluster
 func (r ReplicaSetReconciler) updateConnectionStringSecrets(mdb mdbv1.MongoDBCommunity) error {
 	for _, user := range mdb.GetScramUsers() {
-		secretNamespacedName := types.NamespacedName{Name: user.PasswordSecretName, Namespace: mdb.NamespacedName().Namespace}
+		secretNamespacedName := types.NamespacedName{Name: user.PasswordSecretName, Namespace: mdb.Namespace}
 		pwd, err := secret.ReadKey(r.client, user.PasswordSecretKey, secretNamespacedName)
 		if err != nil {
 			return err
