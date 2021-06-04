f923235f9a4673f6aeb3e826e549d58814e4f39b
Ciprian Tibulca <ciprian.tibulca@mongodb.com>
2021-06-04 09:20:14 +0100

    CLOUDP-89492 Expose connection string for deployments for easy of use (#523)
    * publish connection strings - scram
    
    * expose connection strings - e2e test
    
    * expose connection strings - validate users config

--- controllers/validation/validation.go
+++ controllers/validation/validation.go
@@ -1,13 +1,57 @@
 package validation
 
 import (
+	"fmt"
+	"strings"
+
 	mdbv1 "github.com/mongodb/mongodb-kubernetes-operator/api/v1"
+	"github.com/mongodb/mongodb-kubernetes-operator/pkg/authentication/scram"
 	"github.com/pkg/errors"
 )
 
-func Validate(oldSpec, newSpec mdbv1.MongoDBCommunitySpec) error {
-	if oldSpec.Security.TLS.Enabled && !newSpec.Security.TLS.Enabled {
+// ValidateInitalSpec checks if the resource initial Spec is valid
+func ValidateInitalSpec(mdb mdbv1.MongoDBCommunity) error {
+	if err := validateUsers(mdb); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ValidateUpdate validates that the new Spec, corresponding to the existing one is still valid
+func ValidateUpdate(mdb mdbv1.MongoDBCommunity, oldSpec mdbv1.MongoDBCommunitySpec) error {
+	if oldSpec.Security.TLS.Enabled && !mdb.Spec.Security.TLS.Enabled {
 		return errors.New("TLS can't be set to disabled after it has been enabled")
+	}
+
+	if err := validateUsers(mdb); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// validateUsers checks if the users configuration is valid
+func validateUsers(mdb mdbv1.MongoDBCommunity) error {
+	connectionStringSecretNameMap := map[string]scram.User{}
+	nameCollisions := []string{}
+	for _, user := range mdb.GetScramUsers() {
+		secretName := user.GetConnectionStringSecretName(mdb)
+		if previousUser, exists := connectionStringSecretNameMap[secretName]; exists {
+			nameCollisions = append(nameCollisions,
+				fmt.Sprintf(`[secret name: "%s" for user: "%s", db: "%s" and user: "%s", db: "%s"]`,
+					secretName,
+					previousUser.Username,
+					previousUser.Database,
+					user.Username,
+					user.Database))
+		} else {
+			connectionStringSecretNameMap[secretName] = user
+		}
+	}
+	if len(nameCollisions) > 0 {
+		return errors.Errorf("connection string secret names collision, update at least one of the users so that the resulted secret names (<resource name>-<user>-<db>) are unique: %s",
+			strings.Join(nameCollisions, ", "))
 	}
 
 	return nil
