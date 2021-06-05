0d5b750ed071ce69b6987bbca237f275e19f50fa
chatton <cianhatton@gmail.com>
2021-06-05 20:36:36 +0100

    updated jira query url

--- scripts/dev/open_tickets_for_this_release.py
+++ scripts/dev/open_tickets_for_this_release.py
@@ -19,7 +19,7 @@ def _open_url(url: str) -> None:
 def main() -> int:
     release_version = _load_release()["mongodb-kubernetes-operator"]
     _open_url(
-        f"https://jira.mongodb.org/browse/CLOUDP-88204?jql=project%20%3D%20%22Cloud%20Services%22%20%20AND%20component%20%3D%20%22Kubernetes%20Community%22%20%20And%20fixVersion%20%3D%20kube-community-{release_version}%20AND%20status%20in%20(closed%2C%20resolved)/")
+        f"https://jira.mongodb.org/issues?jql=project%20%3D%20%22Cloud%20Services%22%20%20AND%20component%20%3D%20%22Kubernetes%20Community%22%20%20And%20fixVersion%20%3D%20kube-community-${release_version}%20AND%20status%20in%20(closed%2C%20resolved)")
     return 0
 
 
