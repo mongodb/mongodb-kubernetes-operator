be3d7c7a99be420971aecdad3c5ccb20a72a666d
chatton <cianhatton@gmail.com>
2021-06-06 01:24:27 +0100

    added logs for connection strings and users

--- controllers/mongodb_users.go
+++ controllers/mongodb_users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"go.uber.org/zap"
 
 	mdbv1 "github.com/mongodb/mongodb-kubernetes-operator/api/v1"
 	"github.com/mongodb/mongodb-kubernetes-operator/pkg/kube/secret"
@@ -11,8 +12,9 @@ import (
 
 // ensureUserResources will check that the configured user password secrets can be found
 // and will start monitor them so that the reconcile process is triggered every time these secrets are updated
-func (r ReplicaSetReconciler) ensureUserResources(mdb mdbv1.MongoDBCommunity) error {
+func (r ReplicaSetReconciler) ensureUserResources(mdb mdbv1.MongoDBCommunity, log *zap.SugaredLogger) error {
 	for _, user := range mdb.GetScramUsers() {
+		log.Infof("Ensuring resources for user %s in %s", user.Username, user.Database)
 		secretNamespacedName := types.NamespacedName{Name: user.PasswordSecretName, Namespace: mdb.Namespace}
 		if _, err := secret.ReadKey(r.client, user.PasswordSecretKey, secretNamespacedName); err != nil {
 			if apiErrors.IsNotFound(err) {
@@ -29,8 +31,10 @@ func (r ReplicaSetReconciler) ensureUserResources(mdb mdbv1.MongoDBCommunity) er
 
 // updateConnectionStringSecrets updates secrets where user specific connection strings are stored.
 // The client applications can mount these secrets and connect to the mongodb cluster
-func (r ReplicaSetReconciler) updateConnectionStringSecrets(mdb mdbv1.MongoDBCommunity) error {
+func (r ReplicaSetReconciler) updateConnectionStringSecrets(mdb mdbv1.MongoDBCommunity, log *zap.SugaredLogger) error {
 	for _, user := range mdb.GetScramUsers() {
+		log.Infof("Ensuring connection string secret for user %s in %s", user.Username, user.Database)
+
 		secretNamespacedName := types.NamespacedName{Name: user.PasswordSecretName, Namespace: mdb.Namespace}
 		pwd, err := secret.ReadKey(r.client, user.PasswordSecretKey, secretNamespacedName)
 		if err != nil {
