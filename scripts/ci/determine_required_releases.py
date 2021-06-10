83ba593eec93729f41bceb8c2e11e34455e00000
chatton <cianhatton@gmail.com>
2021-06-10 12:14:23 +0100

    addressed PR feedback

--- scripts/ci/determine_required_releases.py
+++ scripts/ci/determine_required_releases.py
@@ -37,20 +37,21 @@ def _load_image_name_to_version_map() -> Dict:
 def main() -> int:
     if len(sys.argv) != 2:
         raise ValueError("usage: determine_required_releases.py [image-name]")
+    image_name = sys.argv[1]
     image_name_map = _load_image_name_to_version_map()
 
-    if sys.argv[1] not in image_name_map:
+    if image_name not in image_name_map:
         raise ValueError(
             "Unknown image type [{}], valid values are [{}]".format(
-                sys.argv[1], ",".join(image_name_map.keys())
+                image_name, ",".join(image_name_map.keys())
             )
         )
 
-    if sys.argv[1] not in QUAY_URL_MAP:
-        raise ValueError("No associated image url with key [{}]".format(sys.argv[1]))
+    if image_name not in QUAY_URL_MAP:
+        raise ValueError("No associated image url with key [{}]".format(image_name))
 
-    tags = _get_all_released_tags(sys.argv[1])
-    if image_name_map[sys.argv[1]] in tags:
+    tags = _get_all_released_tags(image_name)
+    if image_name_map[image_name] in tags:
         print("released")
     else:
         print("unreleased")
