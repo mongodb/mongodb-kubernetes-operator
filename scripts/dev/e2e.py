fd3f5046c759d080e87a773e333bba501b853cb9
chatton <cianhatton@gmail.com>
2021-06-11 18:48:29 +0100

    build e2e image

--- scripts/dev/e2e.py
+++ scripts/dev/e2e.py
@@ -214,7 +214,7 @@ def wait_for_pod_to_be_running(
         lambda: corev1.read_namespaced_pod(name, namespace),
         lambda pod: pod.status.phase == "Running",
         sleep_time=5,
-        timeout=180,
+        timeout=240,
         exceptions_to_ignore=ApiException,
     ):
         raise Exception("Pod never got into Running state!")
