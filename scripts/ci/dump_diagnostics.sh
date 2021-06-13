affbfab9e909c83a0abfc46d17fac7fb7d903198
chatton <cianhatton@gmail.com>
2021-06-13 20:52:39 +0100

    updated diagnostics script

--- scripts/ci/dump_diagnostics.sh
+++ scripts/ci/dump_diagnostics.sh
@@ -7,8 +7,23 @@ namespace="$1"
 echo "Dumping CRD"
 kubectl get crd mongodbcommunity.mongodbcommunity.mongodb.com -o yaml > diagnostics/crd.yaml
 
+# dump operator deployment information.
+for deployment_name in $(kubectl get deployment -n "${namespace}" --output=jsonpath={.items..metadata.name}); do
+  echo "Writing Deployment describe for deployment ${deployment_name}"
+  kubectl describe deploy "${deployment_name}" > "diagnostics/${deployment_name}.txt"
+
+  echo "Writing Deployment yaml for deployment ${deployment_name}"
+  kubectl get deploy "${deployment_name}" -o yaml > "diagnostics/${deployment_name}.yaml"
+done
+
 # dump logs for every container in every pod in the given namespace
 for pod_name in $(kubectl get pod -n "${namespace}" --output=jsonpath={.items..metadata.name}); do
+  echo "Writing Pod describe for pod ${pod_name}"
+  kubectl describe pod "${pod_name}" > "diagnostics/${pod_name}.txt"
+
+  echo "Writing Pod yaml for pod ${pod_name}"
+  kubectl get pod "${pod_name}" -o yaml > "diagnostics/${pod_name}.yaml"
+
   for container_name in $(kubectl get pods -n "${namespace}" "${pod_name}" -o jsonpath='{.spec.containers[*].name}'); do
       echo "Writing log file for pod ${pod_name} - container ${container_name} to diagnostics/${pod_name}-${container_name}.log"
       kubectl logs -n "${namespace}" "${pod_name}" -c "${container_name}" > "diagnostics/${pod_name}-${container_name}.log";
@@ -31,14 +46,13 @@ for mdbc_name in $(kubectl get mongodbcommunity -n "${namespace}" --output=jsonp
 done
 
 
-
 # dump information about relevant secrets.
 # Skip service account tokens, and also skip the Automation Config as this is handled as a special case above.
 for secret in $(kubectl get secret -n "${namespace}" --output=jsonpath={.items..metadata.name}); do
   if ! echo "${secret}" | grep -qE "token|-config"; then
-    echo "Dumping secret ${secret}"
+    echo "Writing secret ${secret}"
     kubectl get secret "${secret}" -o json | jq -r '.data | with_entries(.value |= @base64d)' > "diagnostics/secrets/${secret}.json"
   else
-    echo "Skipping skipping ${secret}"
+    echo "Skipping secret ${secret}"
   fi
 done
