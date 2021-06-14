55591d10418b5869c99ae5ca3cb25239470d679f
chatton <cianhatton@gmail.com>
2021-06-14 20:13:58 +0100

    added grep for workflow_dispatch

--- scripts/dev/run_e2e_gh.sh
+++ scripts/dev/run_e2e_gh.sh
@@ -6,6 +6,6 @@ current_branch="$(git branch --show-current)"
 
 gh workflow run e2e-dispatch.yml -f "test-name=${test_name}" --ref "${current_branch}"
 
-run_id="$(gh run list --workflow=e2e-dispatch.yml | grep -Eo "[0-9]+" | head -n 1)"
+run_id="$(gh run list --workflow=e2e-dispatch.yml | grep workflow_dispatch | grep -Eo "[0-9]+" | head -n 1)"
 
 gh run view "${run_id}" --web
