c97690f753a6b3f2ce09747e31650557059ec96c
chatton <cianhatton@gmail.com>
2021-06-14 20:12:17 +0100

    Corrected gh command

--- scripts/dev/run_e2e_gh.sh
+++ scripts/dev/run_e2e_gh.sh
@@ -8,4 +8,4 @@ gh workflow run e2e-dispatch.yml -f "test-name=${test_name}" --ref "${current_br
 
 run_id="$(gh run list --workflow=e2e-dispatch.yml | grep -Eo "[0-9]+" | head -n 1)"
 
-gh run view --job="${run_id}" --web
+gh run view "${run_id}" --web
