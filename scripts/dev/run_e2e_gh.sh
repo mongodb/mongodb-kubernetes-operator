b4e97f72f77c7381bd8f0c77b07af745728b5e09
Cian Hatton <cianhatton@gmail.com>
2021-06-15 11:15:40 +0100

    Fix script to launch github action (#565)

--- scripts/dev/run_e2e_gh.sh
+++ scripts/dev/run_e2e_gh.sh
@@ -6,6 +6,9 @@ current_branch="$(git branch --show-current)"
 
 gh workflow run e2e-dispatch.yml -f "test-name=${test_name}" --ref "${current_branch}"
 
-run_id="$(gh run list --workflow=e2e-dispatch.yml | grep workflow_dispatch | grep -Eo "[0-9]+" | head -n 1)"
+echo "Waiting for task to start..."
+sleep 2
+
+run_id="$(gh run list --workflow=e2e-dispatch.yml | grep workflow_dispatch | grep -Eo "[0-9]{9,11}" | head -n 1)"
 
 gh run view "${run_id}" --web
