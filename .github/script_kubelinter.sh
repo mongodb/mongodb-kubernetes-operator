44def8f015d4369808a67175fd035eb5d06c90c7
killian2k <killian.kaempf@gmail.com>
2021-06-30 11:34:21 +0200

    Makes script more readable and add checkout

--- .github/script_kubelinter.sh
+++ .github/script_kubelinter.sh
@@ -1,25 +1,29 @@
-array=( ${HOME}/files_modified.json ${HOME}/files_added.json ${HOME}/files_renamed.json)
+list_json_parent_files=( ${HOME}/files_modified.json ${HOME}/files_added.json ${HOME}/files_renamed.json)
 number_errors=0
-indent() { sed 's/^/    /'; }
-for parent_file in "${array[@]}"
+echo_with_indent() {echo $1 | sed 's/^/   /'}
+get_files_from_json() {cat $1 | jq -r '.[]'}
+file_exits() {[[ -f "$1" ]]}
+
+for parent_file in "${list_json_parent_files[@]}"
 do
-    for file in $(cat $parent_file | jq -r '.[]')
+    for file in $(get_files_from_json $parent_file)
     do
         echo "file:" $file
-        if [[ $file == *.yaml ]] && [[ -f "$file" ]]; then
+        if (file_exits $file) && [[ $file == *.yaml ]]; then
             output_error=$(kube-linter lint $file 2>&1 > /dev/null)
             output=$(kube-linter lint $file 2> /dev/null)
             exit_statut=$?
+            
             if (( $exit_statut > 0)); then
                 number_errors=$((number_errors+1))
-                echo $output | sed 's/^/    /'
+                echo_with_indent $output
             elif [ -n "$output_error" ]; then
-                echo $output_error | indent
+                echo_with_indent $output_error
             else
-                echo "Valid" | indent
+                echo_with_indent "valid"
             fi
         else
-            echo "ignored" | indent
+            echo_with_indent "ignored"
         fi
     done    
 done
