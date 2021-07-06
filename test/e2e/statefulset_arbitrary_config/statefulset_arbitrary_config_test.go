f554508c3ce2784da10847bf9ac78310f22abdde
Florentina Simlinger <florentina.simlinger@mongodb.com>
2021-07-06 10:52:41 +0100

    change parameters to decrease coupling

--- test/e2e/statefulset_arbitrary_config/statefulset_arbitrary_config_test.go
+++ test/e2e/statefulset_arbitrary_config/statefulset_arbitrary_config_test.go
@@ -34,8 +34,8 @@ func TestStatefulSetArbitraryConfig(t *testing.T) {
 
 	mdb.Spec.StatefulSetConfiguration.SpecWrapper.Spec.Template.Spec.Containers[1].ReadinessProbe = &corev1.Probe{TimeoutSeconds: 100}
 
-	serviceName := "database"
-	mdb.Spec.StatefulSetConfiguration.SpecWrapper.Spec.ServiceName = serviceName
+	customServiceName := "database"
+	mdb.Spec.StatefulSetConfiguration.SpecWrapper.Spec.ServiceName = customServiceName
 
 	tester, err := mongotester.FromResource(t, mdb)
 	if err != nil {
@@ -44,7 +44,7 @@ func TestStatefulSetArbitraryConfig(t *testing.T) {
 
 	t.Run("Create MongoDB Resource", mongodbtests.CreateMongoDBResource(&mdb, ctx))
 	t.Run("Basic tests", mongodbtests.BasicFunctionality(&mdb))
-	t.Run("Test setting Service Name", mongodbtests.ServiceWithNameExists(&mdb, serviceName))
+	t.Run("Test setting Service Name", mongodbtests.ServiceWithNameExists(customServiceName, mdb.Namespace))
 	t.Run("Test Basic Connectivity", tester.ConnectivitySucceeds())
 	t.Run("AutomationConfig has the correct version", mongodbtests.AutomationConfigVersionHasTheExpectedVersion(&mdb, 1))
 	t.Run("Container has been merged by name", mongodbtests.StatefulSetContainerConditionIsTrue(&mdb, "mongodb-agent", func(container corev1.Container) bool {
