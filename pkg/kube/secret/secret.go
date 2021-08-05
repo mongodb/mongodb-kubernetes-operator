f511dae84647d8f7200bbee14c0e4633386c2beb
Rajdeep Das <rajdeep.das@mongodb.com>
2021-08-05 15:33:50 +0200

    PR commend: updated comment

--- pkg/kube/secret/secret.go
+++ pkg/kube/secret/secret.go
@@ -134,7 +134,7 @@ func EnsureSecretWithKey(secretGetUpdateCreateDeleter GetUpdateCreateDeleter, ns
 	return string(existingSecret.Data[key]), nil
 }
 
-// CopySecret copies secret object(data) from one cluster to another
+// CopySecret copies secret object(data) from one cluster client to another, the from and to cluster-client can belong to the same or different clusters
 func CopySecret(fromClient Getter, toClient GetUpdateCreator, sourceSecretNsName, destNsName types.NamespacedName) error {
 	s, err := fromClient.GetSecret(sourceSecretNsName)
 	if err != nil {
