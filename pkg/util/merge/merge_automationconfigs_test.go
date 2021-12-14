6ce3aab6bcdae80df59b38fabb1beb0dc1ed536e
Cian Hatton <cianhatton@gmail.com>
2021-12-14 16:48:48 +0000

    added additional unit tsets

--- pkg/util/merge/merge_automationconfigs_test.go
+++ pkg/util/merge/merge_automationconfigs_test.go
@@ -36,3 +36,31 @@ func TestMergeAutomationConfigs(t *testing.T) {
 	assert.True(t, mergedAc.Processes[1].Disabled)
 	assert.False(t, mergedAc.Processes[2].Disabled)
 }
+
+func TestMergeAutomationConfigs_NonExistentMember(t *testing.T) {
+	original, err := automationconfig.NewBuilder().
+		SetName("test-ac").
+		SetMembers(3).
+		Build()
+
+	assert.NoError(t, err)
+	override, err := automationconfig.NewBuilder().
+		SetName("test-ac-0").
+		SetMembers(3).
+		AddProcessModification(func(i int, process *automationconfig.Process) {
+			process.Disabled = i == 1
+		}).Build()
+
+	assert.NoError(t, err)
+
+
+	assert.False(t, override.Processes[0].Disabled)
+	assert.True(t, override.Processes[1].Disabled)
+	assert.False(t, override.Processes[2].Disabled)
+
+	mergedAc := AutomationConfigs(original, override)
+
+	assert.False(t, mergedAc.Processes[0].Disabled)
+	assert.False(t, mergedAc.Processes[1].Disabled, "should not be updated as the name does not match.")
+	assert.False(t, mergedAc.Processes[2].Disabled)
+}
