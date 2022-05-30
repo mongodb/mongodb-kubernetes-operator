300d90e76456d03fc5824b556d57ac594e13fa3d
Łukasz Sierant <lukasz.sierant@mongodb.com>
2022-05-30 10:58:13 +0200

    Create local kind cluster with specified name (#987)
    * Create local kind cluster with specified name
    
    Improved `scripts/dev/setup_kind_cluster.sh`:
      * added switch: -n <cluster_name> to specify kind cluster's
        name
      * added switch: -e to export kind credentials after creating cluster
    
    Improved docs.
    
    * Update sonar commit hash in requirements.txt
    
    * Empty commit

--- scripts/dev/setup_kind_cluster.sh
+++ scripts/dev/setup_kind_cluster.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
 set -Eeou pipefail
 
+function usage() {
+  echo "Deploy local registry and create kind cluster configured to use this registry. Local Docker registry is deployed at localhost:5000.
+
+Usage:
+  setup_kind_cluster.sh [-n <cluster_name>]
+  setup_kind_cluster.sh [-h]
+  setup_kind_cluster.sh [-n <cluster_name>] [-e]
+
+Options:
+  -n <cluster_name>   (optional) Set kind cluster name to <cluster_name>. Creates kubeconfig in ~/.kube/<cluster_name>. The default name is 'kind' if not set.
+  -e                  (optional) Export newly created kind cluster's credentials to ~/.kube/<cluster_name> and set current kubectl context.
+  -h                  (optional) Shows this screen.
+"
+  exit 0
+}
+
+cluster_name=${CLUSTER_NAME:-"kind"}
+export_kubeconfig=0
+while getopts ':n:he' opt; do
+    case $opt in
+      (n)   cluster_name=$OPTARG;;
+      (e)   export_kubeconfig=1;;
+      (h)   usage;;
+      (*)   usage;;
+    esac
+done
+shift "$((OPTIND-1))"
+
+kubeconfig_path="$HOME/.kube/${cluster_name}"
+
 # create the kind network early unless it already exists.
 # it would normally be created automatically by kind but we
 # need it earlier to get the IP address of our registry.
@@ -21,7 +51,7 @@ fi
 ip="$(docker inspect kind-registry -f '{{.NetworkSettings.Networks.kind.IPAddress}}')"
 
 # create a cluster with the local registry enabled in containerd
-cat <<EOF | kind create cluster --kubeconfig ~/.kube/kind --config=-
+cat <<EOF | kind create cluster --name "${cluster_name}" --kubeconfig "${kubeconfig_path}" --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 containerdConfigPatches:
@@ -32,7 +62,7 @@ EOF
 
 # Document the local registry (from  https://kind.sigs.k8s.io/docs/user/local-registry/)
 # https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
-cat <<EOF | kubectl apply -f -
+cat <<EOF | kubectl apply --kubeconfig "${kubeconfig_path}" -f -
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -43,3 +73,7 @@ data:
     host: "localhost:${reg_port}"
     help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
 EOF
+
+if [[ "${export_kubeconfig}" == "1" ]]; then
+  kind export kubeconfig --name "${cluster_name}"
+fi
