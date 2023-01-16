139dddc28b8992abba1514ae029253e4a7437e30
nam <nam.nguyen@datadoghq.com>
2023-01-16 12:08:57 +0100

    increase further

--- scripts/dev/e2e.py
+++ scripts/dev/e2e.py
@@ -129,7 +129,7 @@ def create_test_pod(args: argparse.Namespace, dev_config: DevConfig) -> None:
                         "go",
                         "test",
                         "-v",
-                        "-timeout=40m",
+                        "-timeout=45m",
                         "-failfast",
                         f"./test/e2e/{args.test}",
                     ],
