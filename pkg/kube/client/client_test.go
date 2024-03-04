1e5531b3da19e67509c6b8e2e514898e834643a5
Łukasz Sierant <lukasz.sierant@mongodb.com>
2024-03-04 11:17:40 +0100

    Removed unused code

--- pkg/kube/client/client_test.go
+++ pkg/kube/client/client_test.go
@@ -2,8 +2,6 @@ package client
 
 import (
 	"context"
-	"github.com/mongodb/mongodb-kubernetes-operator/pkg/kube/annotations"
-	"github.com/mongodb/mongodb-kubernetes-operator/pkg/kube/service"
 	"testing"
 
 	"k8s.io/apimachinery/pkg/types"
@@ -82,34 +80,4 @@ func TestDeleteConfigMap(t *testing.T) {
 
 	_, err = client.GetConfigMap(types.NamespacedName{Name: "config-map", Namespace: "default"})
 	assert.Equal(t, err, notFoundError())
-}
-
-// TestSetAnnotationsDoesNotChangeSuppliedObject verifies that the supplied object for annotations.SetAnnotations is not overridden due being a shallow copy.
-// the function lies here, otherwise it will lead to import cycles.
-func TestSetAnnotationsDoesNotChangeSuppliedObject(t *testing.T) {
-	c := NewClient(NewMockedClient())
-	backedService := service.Builder().
-		SetName("some-name").
-		SetNamespace("some-namespace").
-		SetAnnotations(map[string]string{"one": "annotation"}).
-		SetClusterIP("123").
-		Build()
-	err := service.CreateOrUpdateService(c, backedService)
-	assert.NoError(t, err)
-
-	serviceWithoutAnnotation := service.Builder().
-		SetName("some-name").
-		SetNamespace("some-namespace").
-		Build()
-
-	// make sure this method only changes the annotations locally and in kube
-	err = annotations.SetAnnotations(&serviceWithoutAnnotation, map[string]string{"new": "something"}, c)
-	assert.NoError(t, err)
-	assert.Len(t, serviceWithoutAnnotation.Annotations, 2)
-	assert.Equal(t, "", serviceWithoutAnnotation.Spec.ClusterIP)
-
-	err = c.Get(context.TODO(), types.NamespacedName{Name: serviceWithoutAnnotation.GetName(), Namespace: serviceWithoutAnnotation.GetNamespace()}, &serviceWithoutAnnotation)
-	assert.NoError(t, err)
-	assert.Len(t, serviceWithoutAnnotation.Annotations, 2)
-	assert.Equal(t, "123", serviceWithoutAnnotation.Spec.ClusterIP)
 }
